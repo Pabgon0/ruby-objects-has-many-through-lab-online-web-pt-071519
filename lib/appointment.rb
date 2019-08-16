9c891ac4dc7b0a26c70fc51662a093d1b94954aa
Pablo Gonzalez <pabgon0@gmail.com>
2019-08-16 03:58:30 +0000

    Automatically backed up by Learn

--- lib/appointment.rb
+++ lib/appointment.rb
@@ -1,9 +1,10 @@
-
+require 'pry'
 class Appointment
   attr_accessor :date, :patient, :doctor
   @@all = []
   
   def initialize(date, patient, doctor)
+    binding.pry
     @date = date
     @patient = patient
     @doctor = doctor
